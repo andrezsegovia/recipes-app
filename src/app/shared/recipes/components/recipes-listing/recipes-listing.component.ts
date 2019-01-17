d4a9e037a8f8802ac942e12a318e08b02eed8eab
Andrez Segovia <ajsegoviacruz93@gmail.com>
2019-01-16 20:40:47 -0500

    :zap: Listing all recipes component

--- src/app/shared/recipes/components/recipes-listing/recipes-listing.component.ts
+++ src/app/shared/recipes/components/recipes-listing/recipes-listing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from 'src/app/domain/recipe';
 import { RecipeService } from 'src/app/shared/services/recipe.service';
+import { Router, } from '@angular/router';
 
 @Component({
   selector: 'app-recipes-listing',
@@ -10,12 +11,21 @@ import { RecipeService } from 'src/app/shared/services/recipe.service';
 export class RecipesListingComponent implements OnInit {
 
   recipes: Recipe[];
-  constructor(private recipeService: RecipeService ) { }
+  constructor(private recipeService: RecipeService,
+    private router: Router ) { }
 
   ngOnInit() {
     this.recipeService.getAll().subscribe( data => {
-        this.recipes = data;
+        this.recipes = data['recipes'];
     });
+  }
+
+  edit( recipe: Recipe ) {
+    this.router.navigate(['/recipes/edit', recipe.id ]);
+  }
+
+  create( ) {
+    this.router.navigate(['/recipes/create']);
   }
 
 }
