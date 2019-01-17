11c330b46c1fa8d58915d2bcba71e2e5c44222f9
Andrez Segovia <ajsegoviacruz93@gmail.com>
2019-01-16 20:37:04 -0500

    :zap:Developed Find Recipe by Id

--- src/app/shared/services/recipe.service.ts
+++ src/app/shared/services/recipe.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Recipe } from 'src/app/domain/recipe';
 
 @Injectable()
@@ -11,4 +12,23 @@ export class RecipeService {
   getAll(): Observable<Array<Recipe>> {
     return this.http.get<Array<Recipe>>('/assets/recipes.json');
   }
+
+  getById( id: Number ): Observable<Recipe> {
+    return this.http.get<Array<Recipe>>('/assets/recipes.json').pipe(
+        map( res => {
+          let recipe: Recipe;
+          res['recipes'].forEach(element => {
+            if ( element['id'] === id) {
+              recipe = {
+                id: element['id'],
+                name: element['name'],
+                nextDay: element['nextDay'],
+                lastDay: element['lastDay'],
+                time: element['time']
+              };
+            }
+          });
+          return recipe;
+        }));
+    }
 }
